fe12e271ca1287b1f29b44d031f3207fb55dafb8
Armaghan <armaghan.sma@gmail.com>
2024-11-02 18:05:27 +0330

    Initial commit

--- blog_api/settings.py
+++ blog_api/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'dj_rest_auth',
     'dj_rest_auth.registration',
+    'drf_spectacular',
 
     # local
     'accounts',
@@ -163,4 +164,12 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
     ],
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Blog API Project",
+    "DESCRIPTION": "A sample blog about DRF",
+    "VERSION": "1.0.0",
+
 }
--- posts/serializers.py
+++ posts/serializers.py
@@ -4,7 +4,6 @@ from rest_framework import serializers
 from .models import Post
 
 
-
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
